e6d93b6a106d3101db98dd27cc89ba3fe5e5cdb8
AaronCode2 <AaronWorkPC@outlook.com>
2026-09-19 02:32:03 +0100

    added stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#define RAYLIB_NUKLEAR_IMPLEMENTATION
+#include "raylib-nuklear.h"
+
+using Json = nlohmann::json;
+
 // Let's start coding! Press f5 to run
 
 int main() {
